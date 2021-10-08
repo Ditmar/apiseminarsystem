bedb78c5ad8c237f4f28e0b62b88209d77cae987
Ditmaros <ditmarcastro@gmail.com>
2021-10-07 20:56:25 -0400

    advances server

--- src/server.ts
+++ src/server.ts
@@ -1,1 +1,6 @@
-console.log("test");
+import App from "./App";
+
+const application: App = new App();
+application.getApp().listen(application.getPort(), () => {
+  console.log(`Server running in port! ${application.getPort()}`);
+});
